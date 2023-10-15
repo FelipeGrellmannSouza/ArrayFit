1d1393f3553c0d6dd44552589af7921a29b24a6f
FelipeGrellmannSouza <felipegrellmann1@gmail.com>
2023-10-15 01:00:30 -0300

    Verificação de dados preenchidos telaCadastro

--- app/src/main/java/com/example/arrayfit/activity/TelaCadastro.java
+++ app/src/main/java/com/example/arrayfit/activity/TelaCadastro.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.arrayfit.R;
 import com.example.arrayfit.model.Usuario;
+import com.google.firebase.StartupTime;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class TelaCadastro extends AppCompatActivity {
@@ -33,6 +35,52 @@ public class TelaCadastro extends AppCompatActivity {
         edtCelular = findViewById(R.id.edtCelular);
         btnCadastrar = findViewById(R.id.btnCadastrar);
     }
+
+    public void validarCampos(View view){
+        String nome = edtNome.getText().toString();
+        String email = edtEmail.getText().toString();
+        String senha = edtSenha.getText().toString();
+        String celular = edtCelular.getText().toString();
+
+        //Verifica se os campos estão preenchidos
+        if(!nome.isEmpty()){
+            if(!celular.isEmpty()){
+                if (!email.isEmpty()){
+                    if (!senha.isEmpty()){
+
+                        //estanciando objeto usuario
+                        Usuario usuario = new Usuario();
+
+                        usuario.setNome(nome);
+                        usuario.setEmail(email);
+                        usuario.setSenha(senha);
+                        usuario.setCelular(celular);
+                        
+                        //cadastro do usuario
+                        cadastrarUsuario();
+
+
+
+
+
+                    }else{
+                        Toast.makeText(this, "Preencha o campo senha", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(this, "Preencha o campo email", Toast.LENGTH_SHORT).show();
+                }
+            }else{
+                Toast.makeText(this, "Preencha o campo celular", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(this, "Preencha o campo nome", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void cadastrarUsuario() {
+
+    }
+
 
     public void voltarTC (View view){
         finish();
